ae819071a8d769862d3f46d7026cf2a3f5c24503
Zach Dowding <zdowding@gmail.com>
2015-09-07 14:28:14 -0600

    EDMPlugin.java Added
    Signed-off-by: RoboRave <zdowding@gmail.com>

---
+++ src/main/java/mods/common/addon/plugin/test/EDMPlugin.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package mods.common.addon.plugin.test;
+
+import net.minecraft.client.gui.GuiScreen;
+import mods.common.addon.plugin.Plugin;
+import mods.common.addon.plugin.PluginMetadata;
+
+/**
+ * @author Zach
+ *
+ */
+public class EDMPlugin implements Plugin {
+
+	
+	@Override
+	public void preInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	@Override
+	public void postInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PluginMetadata meta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public String desc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean Tabs() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	@Override
+	public Class<? extends GuiScreen> Tabclass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
